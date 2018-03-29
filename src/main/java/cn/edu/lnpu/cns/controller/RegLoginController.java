df00f612e4ea4bed809f1db603fcc403125f48f9
wangning113 <wangning29@jd.com>
2018-03-29 09:50:33 +0800

    设置未登陆跳转至登录页面

--- src/main/java/cn/edu/lnpu/cns/controller/RegLoginController.java
+++ src/main/java/cn/edu/lnpu/cns/controller/RegLoginController.java
@@ -1,5 +1,6 @@
 package cn.edu.lnpu.cns.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +11,10 @@ import java.io.PrintWriter;
 /**
  * Created by wangning113 on 2017/12/29.
  */
-@RestController
+@Controller
 public class RegLoginController {
     @RequestMapping("/login_p")
-    public void login(HttpServletResponse resp) throws IOException {
-        resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        resp.setContentType("application/json;charset=UTF-8");
-        PrintWriter out = resp.getWriter();
-        out.write("{\"status\":\"error\",\"msg\":\"尚未登录，请登录!\"}");
-        out.flush();
-        out.close();
-        //        return new RespBean("error", "尚未登录，请登录!");
+    public String login(HttpServletResponse resp) throws IOException {
+        return "index.html";
     }
 }
