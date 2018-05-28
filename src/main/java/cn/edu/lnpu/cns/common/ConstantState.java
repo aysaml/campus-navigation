25eca2e8296c9878b7cf5749b000786a7c95c83b
wangning113 <wangning29@jd.com>
2018-05-28 21:26:05 +0800

    修复图片更新bug，整合阿里云oss

---
+++ src/main/java/cn/edu/lnpu/cns/common/ConstantState.java
@@ -0,0 +1,46 @@
+package cn.edu.lnpu.cns.common;
+
+/**
+ * 封装状态值和消息的枚举类型
+ *
+ * @author wangning113
+ * @since 2018/5/12
+ */
+public enum ConstantState {
+    /*返回码及返回信息*/
+    LOGIN_SUCCESS(0,"登录成功"),
+    LOGIN_FAIL(1,"登录失败"),
+    SUCCESS(0000 ,"成功"),
+    RUNTIME_ERROR(1000, "运行时错误") ,
+    PARAMETER_ERROR(1001,"请求参数错误"),
+    WINDOW_NONT_FIND(1002,"窗口不存在") ,
+    USER_EXIST(1003,"用户名已存在"),
+
+    DATA_ONT_FOUND(2000,"数据不存在"),
+    DATABASE_OPERATE_ERROR(2001,"数据库操作错误") ,
+
+    STATUS_ERROR(3000,"状态错误"),
+
+    INVALID_DATA(-1, "无效数据")  ,
+    INVALID_STATUS(3001,"无效状态"),
+    UPLOAD_FILE_SUCCESS(4000,"文件上传成功"),
+    UPLOAD_FILE_FAILED(4001,"文件上传失败"),
+    UPLOAD_FILE_PART_FAILED(4002,"部分文件上传失败"),
+    UPLOAD_FILE_EMPTY(4003,"上传文件为空");
+
+    int state ;
+    String desc ;
+    ConstantState(int state, String desc) {
+        this.state = state ;
+        this.desc = desc ;
+    }
+
+    public Integer getCode() {
+        return this.state ;
+    }
+
+    public String getMessage() {
+        return this.desc ;
+    }
+
+    }
---
+++ src/main/java/cn/edu/lnpu/cns/controller/FileUploadController.java
@@ -0,0 +1,86 @@
+package cn.edu.lnpu.cns.controller;
+
+
+import cn.edu.lnpu.cns.common.AliOSSUtil;
+import cn.edu.lnpu.cns.common.ConstantState;
+import cn.edu.lnpu.cns.common.JsonResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ClassUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 文件上传controller(上传至阿里云oss)
+ *
+ * @author wangning113
+ * @since 2018/5/28
+ */
+@RestController
+public class FileUploadController {
+
+  private static Logger logger = LoggerFactory.getLogger(FileUploadController.class);
+  /**
+   * 本地存放目录
+   */
+  private static String uoloadPath = ClassUtils.getDefaultClassLoader().getResource("").getPath() + "upload/";
+
+  @PostMapping("/upload")
+  public JsonResult imageUpload(HttpServletRequest request, @RequestParam("file") MultipartFile file) {
+    JsonResult result = new JsonResult();
+    if (!file.isEmpty()) {
+      try {
+        // 上传文件信息
+        logger.info("OriginalFilename：" + file.getOriginalFilename());
+        logger.info("ContentType：" + file.getContentType());
+        logger.info("Name：" + file.getName());
+        logger.info("Size：" + file.getSize());
+        //TODO:文件大小、名称、类型检查的业务处理
+
+        // 检查上传目录
+        File targetFile = new File(uoloadPath);
+        if (!targetFile.exists()) {
+          targetFile.mkdirs();
+        }
+
+        // 实例化输出流
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(uoloadPath + file.getOriginalFilename()));
+        out.write(file.getBytes());
+        out.flush();
+        out.close();
+
+        // 上传到OSS
+        String url = AliOSSUtil.uploadLocalFile(new File(uoloadPath + file.getOriginalFilename()), "/upload/avatar/");
+        if (url == null) {
+          result.setState(ConstantState.UPLOAD_FILE_FAILED.getCode());
+          result.setMessage(ConstantState.UPLOAD_FILE_FAILED.getMessage());
+          result.setData(null);
+          return result;
+        }
+        logger.info("上传完毕,访问地址:"+url);
+        result.setState(ConstantState.UPLOAD_FILE_SUCCESS.getCode());
+        result.setMessage(ConstantState.UPLOAD_FILE_SUCCESS.getMessage());
+        result.setData("https://"+url);
+        return result;
+      } catch (IOException e) {
+        e.printStackTrace();
+        result.setState(ConstantState.UPLOAD_FILE_FAILED.getCode());
+        result.setMessage(ConstantState.UPLOAD_FILE_FAILED.getMessage());
+        result.setData(null);
+        return result;
+      }
+    }
+    result.setState(ConstantState.UPLOAD_FILE_EMPTY.getCode());
+    result.setMessage(ConstantState.UPLOAD_FILE_EMPTY.getMessage());
+    result.setData(null);
+    return result;
+  }
+}
